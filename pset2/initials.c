83ced7f81631f90aaccb094352d80af7611fd9f3
Justin Hill <jrhill85@gmail.com>
2017-07-23 22:03:17 +0000

    add initials problem solution

--- pset2/initials.c
+++ pset2/initials.c
@@ -1,22 +1,47 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include <cs50.h>
+
 // http://docs.cs50.net/problems/initials/more/initials.html
 
 int main(void)
 {
-    // Get name from user. Per requiements, no input helper
+    // Get name from user. Per requirements, don't show any input prompt
+    // DO handle leading and trailing spaces
+    // DON'T handle special characters e.g. .,'-
     string name = get_string();
     
-    // Get leading characters of each non-space sequence
-    string initials = "";
+    // used for traversing string, start at -1 to increment later
+    int i = -1;
     
-    // Capitalize and output initials
-    printf("input name: %s\n",name);
-    printf("initials: %s\n", initials);
-}
-
-string pop_initial(string name);
-{
+    // Print initials for all character groups separated by spaces
+    do
+    {
+        char initial;
+        
+        // Find index of first non-space character
+        do
+        {
+            i++;
+            initial = name[i];
+        }
+        while (initial != '\0' && initial == ' ');
+        
+        // If string hasn't ended, print the initial as a capital
+        if (initial != '\0')
+        {
+            printf("%c", toupper(initial));
+        }
+        
+        // Skip ahead to next space or end of string
+        while (name[i] != '\0' && name[i] != ' ')
+        {
+            i++;
+        }
+    }
+    while (name[i] != '\0');
     
+    // End and print a newline
+    printf("\n");
 }
